2b8c3b832145ea2c1d5f3f4228f1886d582004dc
schnell3526 <schnelldickheiter@gmail.com>
2021-08-20 23:01:58 +0900

    get api_key

--- src/main.go
+++ src/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type apiKey struct {
+	API_KEY    string
+	SECRET_KEY string
+}
 
 func main() {
-	fmt.Println("hello world")
+	raw, err := ioutil.ReadFile("../.config")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	var key apiKey
+
+	json.Unmarshal(raw, &key)
+
+	fmt.Printf("API_KEY = %s\n", key.API_KEY)
+	fmt.Printf("SECRET_KEY = %s\n", key.SECRET_KEY)
 }
